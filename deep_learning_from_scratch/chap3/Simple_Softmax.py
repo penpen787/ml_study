676af6a78433e617ebbc773215d993931f6e2f0a
Kyunghun Jeon <penpen787@gmail.com>
2017-02-08 19:16:45 +0900

    p.92 simple softmax function complete

--- deep_learning_from_scratch/chap3/Simple_Softmax.py
+++ deep_learning_from_scratch/chap3/Simple_Softmax.py
@@ -12,3 +12,12 @@ print(sum_exp_a)
 
 y = exp_a / sum_exp_a
 print(y)
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+b = np.array([0.3, 2.9, 4.0])
+print(softmax(b))
