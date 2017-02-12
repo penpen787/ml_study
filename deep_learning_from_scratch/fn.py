f126343900860cb7569bdb83db62d662e6eb0cd5
Kyunghun Jeon <penpen787@gmail.com>
2017-02-12 16:57:50 +0900

    p.115 cross entropy error function

--- deep_learning_from_scratch/fn.py
+++ deep_learning_from_scratch/fn.py
@@ -10,3 +10,9 @@ def softmax(a):
     exp_a = np.exp(a - max_value)
     sum_exp_a = np.sum(exp_a)
     return exp_a / sum_exp_a
+
+# 교차 엔트로피 오차 (CEE)
+def cross_entropy_error(y, t):
+    delta = 1e-7  # np.log 에  0 입력시, 마이너스 무한대(-inf)가 출력되기 때문에, 아주 작은 값을 더해 0이 되지 않도록 함
+    return -np.sum(t * np.log(y + delta))
+
