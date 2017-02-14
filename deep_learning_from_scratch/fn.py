9677f161e791aaafc7251039d99f7d355d426f6b
Kyunghun Jeon <penpen787@gmail.com>
2017-02-14 11:37:28 +0900

    p.118 cross entropy 2

--- deep_learning_from_scratch/fn.py
+++ deep_learning_from_scratch/fn.py
@@ -16,6 +16,28 @@ def cross_entropy_error(y, t):
     delta = 1e-7  # np.log 에  0 입력시, 마이너스 무한대(-inf)가 출력되기 때문에, 아주 작은 값을 더해 0이 되지 않도록 함
     return -np.sum(t * np.log(y + delta))
 
+# 교차 엔트로피 오차 (CEE)
+# 미니배치도 사용가능하도록 변경
+# for one-hot encoding
+def cross_entropy_error_one_hot(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y)) / batch_size
+
+# 교차 엔트로피 오차 (CEE)
+# 미니배치도 사용가능하도록 변경
+# for one-hot encoding
+def cross_entropy_error_number_label(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
 # 평균 제곱 오차 (MSE)
 def mean_squred_error(y, t):
     return 0.5 * np.sum((y-t)**2)
