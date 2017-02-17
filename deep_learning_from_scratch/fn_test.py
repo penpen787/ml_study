486a9ded937d9ce2735c7c57a01d36df9139f90f
Kyunghun Jeon <penpen787@gmail.com>
2017-02-17 17:57:10 +0900

    코드정리

--- deep_learning_from_scratch/fn_test.py
+++ deep_learning_from_scratch/fn_test.py
@@ -1,5 +1,6 @@
 # 책에 나오는 습작용 공식들 정리
 import numpy as np
+import matplotlib.pylab as plt
 
 # 미분의 나쁜 예
 # h 를 가급적 작은 값을 넣고 싶어 10e-50 을 대입함
@@ -12,7 +13,6 @@ print(np.float32(10e-50))  # 0.0, 너무 작은값을 쓰면 컴퓨터로 계산
 # h 를 10e-4 로 변경함
 print(np.float32(10e-4))
 
-
 # 중심차분 or 중앙차분 : x 를 중심으로 그 전후의 차분을 계산함 (x+h), (x-h)
 def numerical_diff_middle(f, x):
     h = 1e-4  # 0.001
@@ -22,8 +22,6 @@ def numerical_diff_middle(f, x):
 # y = 0.01x^2 + 0.1x
 def function_1(x):
     return 0.01*x**2 + 0.1*x
-
-import matplotlib.pylab as plt
 
 x = np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
