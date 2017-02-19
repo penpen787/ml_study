879ad3e177bb21ab8bf9a7918168a49f41448843
Kyunghun Jeon <penpen787@gmail.com>
2017-02-19 18:40:47 +0900

    P128 기울기

--- deep_learning_from_scratch/fn.py
+++ deep_learning_from_scratch/fn.py
@@ -41,3 +41,21 @@ def cross_entropy_error_number_label(y, t):
 # 평균 제곱 오차 (MSE)
 def mean_squred_error(y, t):
     return 0.5 * np.sum((y-t)**2)
+
+def numberical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+    # f(x+h) 계산
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+
+    # f(x-h) 계산
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2 * h)
+    x[idx] = tmp_val  # 값 복원
+    return grad
