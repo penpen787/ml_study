00637376f23bfb551a4d320753c9d1cae7eb46ec
Kyunghun Jeon <penpen787@gmail.com>
2017-02-27 00:50:30 +0900

    도쿄커밋3 SimpleNet test

---
+++ deep_learning_from_scratch/chap4/Simple_Net_Test.py
@@ -0,0 +1,23 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import fn
+
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)  # 정규분포로 초기화
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = fn.softmax(z)
+        loss = fn.cross_entropy_error(y, t)
+
+        return loss
+
+net = SimpleNet()
+print(net.W) # 램덤 & 정규화된 가중치 매개변수
+
+
