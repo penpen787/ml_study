eb4c68c078690c892321721d5dd5aafab043bdd6
Kyunghun Jeon <penpen787@gmail.com>
2017-03-05 16:26:23 +0900

    p 137 - 2

--- deep_learning_from_scratch/chap4/Two_Layer_Net.py
+++ deep_learning_from_scratch/chap4/Two_Layer_Net.py
@@ -1,7 +1,13 @@
 import sys, os
 sys.path.append(os.pardir)
+import numpy as np
 from fn import *
 
 class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size,
+                 weight_init_std=0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
 
 
